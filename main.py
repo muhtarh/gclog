581761bf5d11adef55bdd4a5b4cedaa7edfe2d35
muhtarh <muhtar.hartopo@gdplabs.id>
2018-10-19 21:55:47 +0700

    add logs and chart

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 import chart_generator as chart
 
-root = '/home/muhtar-hartopo/playground/go-exp/gc/logs'
+root = '/home/muhtar-hartopo/playground/go-exp/gc/logs/go'
 for root, dirs, files in os.walk(root):
     for filename in files:
         filePath = os.path.join(root, filename)
-        chart.generate_chart(filePath, filename)
+        chart_title = 'Message Size ' + filename
+        chart.generate_chart(filePath, chart_title)
